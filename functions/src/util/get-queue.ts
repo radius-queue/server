aeaed54da9fee892208d58e4813713e1a74c30a2
Samuel Berensohn <samberensohn@gmail.com>
2020-07-29 14:47:36 -0700

    QueueInfo Getter Redone

--- functions/src/util/get-queue.ts
+++ functions/src/util/get-queue.ts
@@ -1,4 +1,4 @@
-import {Queue, queueConverter, QueueInfo, diff_minutes} from './queue';
+import {Queue, Party, QueueInfo, diff_minutes} from './queue';
 import {firestore} from '../firestore';
 import { FirebaseError } from 'firebase-admin';
 
@@ -9,12 +9,16 @@ import { FirebaseError } from 'firebase-admin';
 export default async function getQueue(uid : string) {
   let ret: Queue | undefined;
   await firestore.collection('queues').doc(uid)
-      .withConverter(queueConverter)
       .get().then(function(doc: FirebaseFirestore.DocumentData) {
         if (doc.exists) {
-          const q: Queue | undefined = doc.data();
-          // Use a City instance method
-          ret = q;
+          const data = doc.data();
+          ret = {
+            name: data.name,
+            end: data.end.toDate(),
+            uid: '',
+            open: data.open,
+            parties: data.parties.map((party: any)=> Party.fromFirebase(party)),
+          }
         } else {
           console.log('No such document!');
         }
@@ -30,25 +34,23 @@ export default async function getQueue(uid : string) {
 }
 
 export async function getQueueInfo(uid : string) {
-  let ret : QueueInfo = {
-    length: 0,
-    longestWaitTime : 0,
-    open: true,
-  };
+  let ret : QueueInfo | undefined;
   await firestore.collection('queues').doc(uid)
       .get().then(function(doc: FirebaseFirestore.DocumentData) {
         if (doc.exists) {
-          ret.length = doc.data().parties.length;
-          ret.longestWaitTime = diff_minutes(doc.data().parties[0].checkIn.toDate(), new Date())
-          ret.open = doc.data().open;
-
-          // Use a City instance method
+          const data = doc.data();
+          ret = {
+            length: data.parties.length,
+            longestWaitTime: diff_minutes(data.parties[0].checkIn.toDate(), new Date()),
+            open: data.open,
+          }
         } else {
           console.log('No such document!');
         }
       }).catch(function(error: FirebaseError) {
         console.log('Error getting document:', error);
       });
+      
   return ret;
 }
 
