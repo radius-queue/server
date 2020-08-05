9c1cca2bae63f332dff4bf3f88598e62ae392ad0
Samuel Berensohn <samberensohn@gmail.com>
2020-08-04 17:13:02 -0700

    Authorization

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -25,6 +25,11 @@ import {Business, BusinessLocation} from './util/business';
 import {Customer} from './util/customer';
 import { Queue, Party, QueueInfo } from './util/queue';
 
+/**
+ * Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+ * The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+ * `Authorization: Bearer <Firebase ID Token>`.
+ */
 const authentication = async (req: express.Request, res: express.Response, next: () => any) => {
   if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
     res.status(403).send('Unauthorized');
